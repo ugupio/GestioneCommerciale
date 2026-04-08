13947cb75f818d79103f823dba9a65c706ca81d4
defaultuser0 <defaultuser0@GUCCI-NB>
2026-04-08 18:49:22 +0200

    Aggiungere i file di progetto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,35 @@
+﻿namespace GestioneCommerciale.Models
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string RagSociale { get; set; }
+        public string Referente { get; set; }
+        public string Ind_legale { get; set; }
+        public string CAP_Legale { get; set; }
+        public string Citta_Legale { get; set; }
+        public string Prov_Legale { get; set; }
+        public string Telefono { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Cel1 { get; set; }
+        public string Cel2 { get; set; }
+        public string PIva { get; set; }
+        public string CodFiscale { get; set; }
+        public string SDI { get; set; }
+        public string Banca { get; set; }
+        public string Agenzia { get; set; }
+        public string ABI { get; set; }
+        public string CAB { get; set; }
+        public string IBAN { get; set; }
+        public string PagProposto { get; set; }
+        public string PagAccettato { get; set; }
+        public string InfoCommerciali { get; set; }
+        public int PrevisioneAnnuo { get; set; }
+        public string Annotazioni { get; set; }
+        public decimal ScontoProfili { get; set; }
+        public decimal ScontoAccessori { get; set; }
+        public bool Carrello { get; set; }
+        public bool ScaricoAgevole { get; set; }
+    }
+}
---
+++ Models/Ordine.cs
@@ -0,0 +1,30 @@
+﻿namespace GestioneCommerciale.Models
+{
+    public class Ordine
+    {
+        public int IDOrdine { get; set; }
+        public int IDCliente { get; set; }
+        public DateTime DataOrdine { get; set; } = DateTime.Now;
+        public string NumeroDocumento { get; set; }
+        public string StatoOrdine { get; set; }
+        public decimal TotaleImponibile { get; set; }
+        public decimal TotaleIva { get; set; }
+    }
+
+    // Questa è la classe fondamentale per l'inserimento
+    public class OrdineTestata : Ordine
+    {
+        // È qui che definiamo la lista delle righe!
+        public List<DettaglioOrdine> Righe { get; set; } = new List<DettaglioOrdine>();
+    }
+
+    public class DettaglioOrdine
+    {
+        public int IDOrdine { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Quantita { get; set; }
+        public decimal PrezzoUnitario { get; set; }
+        public decimal ScontoRiga { get; set; }
+        public decimal PrezzoTotaleRiga { get; set; }
+    }
+}
